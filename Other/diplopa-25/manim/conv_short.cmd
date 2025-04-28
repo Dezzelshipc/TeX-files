8320498483f9fe8252b77d7477d09731e3fbc49f
Dezzelshipc <foler126@gmail.com>
2025-04-28 20:46:24 +1000

    split2

---
+++ Other/diplopa-25/manim/conv_short.cmd
@@ -0,0 +1,3 @@
+manim-slides convert Intro DiplomIntro ModelsIntro StabilityTypes SignStability ExampleFlow1Intro ExampleFlow1 Outro present/prepre.html
+
+manim-slides convert Intro DiplomIntro ModelsIntro StabilityTypes SignStability ExampleFlow1Intro ExampleFlow1 Outro present/prepre.pptx
